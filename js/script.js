c6747b5763db3256358520956afc2f811f35fa06
gojo-2405 <anonymoushacker24110509@gmail.com>
2026-03-01 20:35:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-function showPage(pageId) {
-  document.querySelectorAll('section').forEach(sec => sec.classList.add('hidden'));
-  document.getElementById(pageId).classList.remove('hidden');
-}
+<script>
+  function showPage(pageId) {
+    // Hide all sections
+    document.querySelectorAll('main section').forEach(sec => sec.classList.add('hidden'));
+    // Show the clicked section
+    document.getElementById(pageId).classList.remove('hidden');
+  }
+
+  // Show Home by default when the page loads
+  window.onload = function() {
+    showPage('home');
+  };
+</script>
